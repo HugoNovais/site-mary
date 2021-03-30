29304c45b01a05500d31e485a7c1856d1a83aaff
Brenda Ferreira <brendaferreirasantos26@gmail.com>
2021-03-30 17:22:51 -0300

    mudei
    mudei

---
+++ java.js
@@ -0,0 +1,19 @@
+function mostrar() {
+    document.getElementById("mostrar").style.display = 'block';
+}
+
+function menu(){
+  let modal = document.querySelector('.modal')
+
+  modal.style.display='block';
+}
+
+function fechar (){
+    let modal= document.querySelector('.modal')
+
+    modal.style.display='none';
+}
+
+function tira() {
+    document.getElementsByClassName("emmo").style.display = 'block';
+}
